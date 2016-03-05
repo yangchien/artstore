20dde2cd675dbd4475744902c14bd4c100d4ddaa
Chuck <tkuttm@gmail.com>
2016-03-05 23:35:07 +0800

    xxx(自訂的訊息)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 u = User.new
-u.email = "admin@test.com"           # 可以改成自己的 email
+u.email = "tkuttm@gmail.com"           # 可以改成自己的 email
 
-u.password = "12345678"              # 最少要八碼
+u.password = "n224417921"              # 最少要八碼
 
-u.password_confirmation = "12345678" # 最少要八碼
+u.password_confirmation = "n224417921" # 最少要八碼
 
 u.is_admin = true
 u.save
